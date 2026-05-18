6ce92b8ff53be7c18cf5e428e2ab3ba54f2b1839
nida <nidhfz85@gmail.com>
2026-05-18 15:10:48 +0500

    upd

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -2,8 +2,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
+# from langchain.chat_models import init_chat_model
 
-model = init_chat_model("gpt-5.4")
-response = model.invoke("Why do parrots talk?")
+# model = init_chat_model("gpt-5.4")
+# response = model.invoke("What is cricket?")
+# print(response.content)
+
+
+from langchain_mistralai import ChatMistralAI
+
+model = ChatMistralAI(model = "mistral-small-2506",temperature=0,max_tokens=20)
+response = model.invoke("Write a poem on computer?")
 print(response.content)
