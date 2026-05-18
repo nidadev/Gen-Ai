b21c469e4bbf37209bc24fb776aa4194ad215b5f
nida <nidhfz85@gmail.com>
2026-05-18 20:30:51 +0500

    upd

--- chatmodels/huggingface.py
+++ chatmodels/huggingface.py
@@ -1,12 +1,11 @@
-from dotenv import load_dotenv
-
-load_dotenv()
 
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 
 llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1",
-    temperature=0.7,
-    max_length=1024,
+   
 )
+model = ChatHuggingFace(llm=llm)
+response = model.invoke("Write a poem on computer?")
+print(response.content)
