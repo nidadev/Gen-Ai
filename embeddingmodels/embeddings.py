f6aeeed5e4b75aaa8f096a70c11ccef92dd993a0
nida <nidhfz85@gmail.com>
2026-05-19 11:54:27 +0500

    upd

--- embeddingmodels/embeddings.py
+++ embeddingmodels/embeddings.py
@@ -6,6 +6,12 @@ embeddings = OpenAIEmbeddings(
     model = "text-embedding-3-small",
     dimensions=64
 )
+texts = [
+    'what are you doing?'
+    'hello this is github'
+]
 
-vector = embeddings.invoke("Write a essay on computer?")
+vector = embeddings.embed_documents(texts)
+#vector = embeddings.embed_query("write a script on grandfather")
+
 print(vector)
